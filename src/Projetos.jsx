a2ecd5f409d9146d797d5fb24125056bf1e4d79a
Guilherme Lima <glhrmdev@gmail.com>
2025-06-09 22:39:48 -0300

    bugfix de case sensitive no nome do arquivo css

--- src/Projetos.jsx
+++ src/Projetos.jsx
@@ -1,4 +1,4 @@
-import "./projetos.css";
+import "./Projetos.css";
 import Footer from "./componentes/components/Footer";
 import Header from "./componentes/components/Header";
 import Landing from "./componentes/components/Landing";
